c76ea0a8b13a36d8fa6cdbb942bd1435b6800bc1
heo <admin@admin.com>
2024-11-17 21:57:28 +0900

    drf_generic

--- todo/urls.py
+++ todo/urls.py
@@ -1,8 +1,10 @@
 from django.contrib import admin
 from django.urls import path
 
-from todo.views import todo_list
+from todo.views import TodoGenericsListAPI, TodoRetrieveUpdateDestroyAPI, todo_list
 
 urlpatterns = [
-    path("",todo_list) #127.0.0.1:8000/todo/list/
+    path("",todo_list), #127.0.0.1:8000/todo/list/
+    path("generics/list", TodoGenericsListAPI.as_view(), name=""),
+    path("generics/<int:pk>", TodoRetrieveUpdateDestroyAPI.as_view(), name=""),
 ]
--- todo/views.py
+++ todo/views.py
@@ -1,7 +1,24 @@
+
 from django.http import HttpResponse
 from django.shortcuts import render
+from rest_framework import generics
+
+from todo.models import Todo
+from todo.serializers import TodoSerializer
+
 
 # Create your views here.
 def todo_list(request):
     return HttpResponse("투두 리스트")
-    return render(request, "todo/list.html")
+    #return render(request, "todo/list.html")
+
+
+class TodoGenericsListAPI(generics.ListAPIView):
+    queryset = Todo.objects.all()
+    serializer_class = TodoSerializer
+
+#delete update retrieve  3개를 담고 있음
+#포스트 맨으로 method 를 위의 3개중 한개로 보내면 됨
+class TodoRetrieveUpdateDestroyAPI(generics.RetrieveUpdateDestroyAPIView):
+    queryset = Todo.objects.all()
+    serializer_class = TodoSerializer
