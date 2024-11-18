fb979b039847f804b991bb51d6d88ab1a5bdd0c8
TEACHER\ICT02_22 <admin@admin.com>
2024-11-18 14:58:03 +0900

    commit

--- todo/urls.py
+++ todo/urls.py
@@ -1,10 +1,15 @@
 from django.contrib import admin
-from django.urls import path
+from django.urls import include, path
 
-from todo.views import TodoGenericsListAPI, TodoRetrieveUpdateDestroyAPI, todo_list
+from todo.views import TodoGenericsListAPI, TodoRetrieveUpdateDestroyAPI, TodoViewSet, todo_list
+from rest_framework.routers import DefaultRouter
+
+router = DefaultRouter()
+router.register("",TodoViewSet)
 
 urlpatterns = [
-    path("",todo_list), #127.0.0.1:8000/todo/list/
+    path("",todo_list), #127.0.0.1:8000/todo/list/    
     path("generics/list", TodoGenericsListAPI.as_view(), name=""),
     path("generics/<int:pk>", TodoRetrieveUpdateDestroyAPI.as_view(), name=""),
+    path("viewsets/",include(router.urls)),
 ]
